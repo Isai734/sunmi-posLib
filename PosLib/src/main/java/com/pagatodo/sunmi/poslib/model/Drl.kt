85acffe9f437c010cf708dfeb5258d8b30e18b3a
Isai Castro <isaicastro60@gmail.com>
2021-02-08 09:23:43 -0600

    Delete ByteUtilJava

--- PosLib/src/main/java/com/pagatodo/sunmi/poslib/model/Drl.kt
+++ PosLib/src/main/java/com/pagatodo/sunmi/poslib/model/Drl.kt
@@ -1,7 +1,6 @@
 package com.pagatodo.sunmi.poslib.model
 
 import com.pagatodo.sunmi.poslib.util.ByteUtil
-import com.pagatodo.sunmi.poslib.util.ByteUtilJava
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.sunmi.pay.hardware.aidlv2.bean.DrlV2
@@ -47,14 +46,14 @@ data class Drl(
 ) {
     fun toDrlV2(): DrlV2 {
         return DrlV2().apply {
-            this@Drl.programID?.also { programID = ByteUtilJava.hexStr2Bytes(it) }
+            this@Drl.programID?.also { programID = ByteUtil.hexStr2Bytes(it) }
             isDefaultLmt = this@Drl.isDefaultLmt
             zeroCheck = this@Drl.zeroCheck.toByte()
-            this@Drl.termClssLmt?.also { termClssLmt = ByteUtilJava.hexStr2Bytes(it) }
-            this@Drl.termClssFloorLmt?.also { termClssFloorLmt = ByteUtilJava.hexStr2Bytes(it) }
+            this@Drl.termClssLmt?.also { termClssLmt = ByteUtil.hexStr2Bytes(it) }
+            this@Drl.termClssFloorLmt?.also { termClssFloorLmt = ByteUtil.hexStr2Bytes(it) }
             statusCheck = this@Drl.statusCheck
-            this@Drl.cvmLmt?.also { cvmLmt = ByteUtilJava.hexStr2Bytes(it) }
-            this@Drl.termFloorLmt?.also { termFloorLmt = ByteUtilJava.hexStr2Bytes(it) }
+            this@Drl.cvmLmt?.also { cvmLmt = ByteUtil.hexStr2Bytes(it) }
+            this@Drl.termFloorLmt?.also { termFloorLmt = ByteUtil.hexStr2Bytes(it) }
             cvmLmtActivate = this@Drl.cvmLmtActivate
             termClssFloorLmtActivate = this@Drl.termClssFloorLmtActivate.toByte()
             termClssLmtActivate = this@Drl.termClssLmtActivate
