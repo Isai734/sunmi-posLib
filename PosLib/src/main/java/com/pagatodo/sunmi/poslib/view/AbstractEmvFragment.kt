66d77c86876d35fc2247a7382c3c339ac053ad50
PCSMEXICO <estradadg7@gmail.com>
2022-03-31 11:24:35 -0600

    static list Test wit new months msidialog

--- PosLib/src/main/java/com/pagatodo/sunmi/poslib/view/AbstractEmvFragment.kt
+++ PosLib/src/main/java/com/pagatodo/sunmi/poslib/view/AbstractEmvFragment.kt
@@ -457,7 +457,8 @@ abstract class AbstractEmvFragment: Fragment(), SunmiTrxListener<AbstractRespues
 
     override fun showDialogMsi(cardNo: String, doContinue: (Boolean) -> Unit) {
         //val msiList = PciUtils.haveMsi(cardNo)
-        var msiList = listOf<Int>(3,6,9,12,18,24)
+
+        var msiList = listOf<Int>(3,6,9,12,18,24,32,48)
         Log.d("msiList", "Lista $msiList")
 
 
