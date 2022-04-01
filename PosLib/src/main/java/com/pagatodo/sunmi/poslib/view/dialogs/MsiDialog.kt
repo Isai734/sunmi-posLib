727a29a1a22450d7de9a71a70488c9dfdf9339b4
PCSMEXICO <estradadg7@gmail.com>
2022-03-31 19:57:35 -0600

    send Serializable data to msiDialog

--- PosLib/src/main/java/com/pagatodo/sunmi/poslib/view/dialogs/MsiDialog.kt
+++ PosLib/src/main/java/com/pagatodo/sunmi/poslib/view/dialogs/MsiDialog.kt
@@ -10,12 +10,13 @@ import com.pagatodo.sunmi.poslib.R
 import com.pagatodo.sunmi.poslib.databinding.FragmentMsiDialogBinding
 import com.pagatodo.sunmi.poslib.model.Msi
 import com.pagatodo.sunmi.poslib.view.adapter.MsiAdapter
+import java.io.Serializable
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.util.ArrayList
 
-class MsiDialog: DialogFragment(R.layout.fragment_msi_dialog) {
+class MsiDialog: DialogFragment(R.layout.fragment_msi_dialog), Serializable {
 
     private lateinit var binding: FragmentMsiDialogBinding
     private val singlePayment: Int = 1
@@ -27,12 +28,14 @@ class MsiDialog: DialogFragment(R.layout.fragment_msi_dialog) {
         val args = this.arguments
         val inputDataAmount = args?.get("amount")
         val inputMsiList = args?.get("msilist")
+        val inputMsiListSerializable = args?.getSerializable("msilist")
 
         binding.tvAmount.text = inputDataAmount.toString()
         val amount1: String = inputDataAmount.toString()
         val amount: Double = amount1.toDouble()
 
-        val firstArray = arrayOf(3,6,9,12,18,24,32)
+        val firstArray = arrayOf(3,6,9,12,18,24)
+        Log.d("msilist", "Serializable: $inputMsiListSerializable")
         val finalListMonth = generateMonthList(firstArray,amount)
 
         binding.recycler.adapter = MsiAdapter(
@@ -69,8 +72,10 @@ class MsiDialog: DialogFragment(R.layout.fragment_msi_dialog) {
         fun create(totalAmount: String, msiList: List<Int>,doContinue: (Boolean) -> Unit): MsiDialog{
             val args = Bundle()
             args.putString("amount",totalAmount)
-            args.putStringArray("msilist",msiList.map { it.toString() }.toTypedArray())
-            //args.putParcelableArrayList("msilist", msiList as ArrayList<out Parcelable>)
+            //args.putStringArray("msilist",msiList.map { it.toString() }.toTypedArray())
+            args.putSerializable("msilist", msiList as Serializable)
+            //args.putParcelableArrayList("msilist",msiList)
+            //args.putParcelableArrayList("msilist", ArrayList() <? extends Parcelable> msiList)
             Log.d("msiList", "create $msiList")
             var msiDialog = MsiDialog()
             msiDialog.arguments = args
